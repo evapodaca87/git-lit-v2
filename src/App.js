f635548b7fb62aa8d0ff95e1f8db032854587b55
evapodaca87 <evapodaca87@gmail.com>
2018-11-26 14:15:58 -0700

    added homepage styling and buttons

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LoginButton from './Components/loginbutton.js';
+import SignUpButton from './Components/signupbutton.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='App'>
+                <div className='head'>
+                    <img
+                        className='logo'
+                        src='https://files.slack.com/files-pri/T1T555TL0-FEBL8TWRX/screen_shot_2018-11-26_at_11.26.57_am.png'
+                    />
+                </div>
+                <p>
+                    Git-Lit is a social networking app designed to help you find drinking buddies based on your
+                    location!
+                </p>
+                <p>
+                    Sign up for an account today, we will use your location to match you with closest Git-Lit users and
+                    "Git-Lit" establishments
+                </p>
+                <p>Signing up is easy! & FREE!</p>
+                <div className='buttons'>
+                    <LoginButton />
+                    <SignUpButton />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Components/loginbutton.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+
+const LoginButton = () => <Button>Login</Button>;
+
+export default LoginButton;
