93c62928f1e840d096ca8c4f6513d6c8dc636a64
evapodaca87 <evapodaca87@gmail.com>
2018-11-26 16:22:08 -0700

    fixed styling

--- src/Home.js
+++ src/Home.js
@@ -11,7 +11,9 @@ class Home extends Component {
                         className='logoHead'
                         src='http://profilepicturesdp.com/wp-content/uploads/2018/07/profile-default-picture-4.png'
                     />
-                    <ToggleBox color='red' />
+                        <ToggleBox />
+                    
+
                     <img
                         className='logoHead'
                         src='https://files.slack.com/files-pri/T1T555TL0-FEBL8TWRX/screen_shot_2018-11-26_at_11.26.57_am.png'
