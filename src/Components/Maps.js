2da6af100f8424b670109b1324ccc0e14bf88f12
jonathanhovland <jonathan.hovland@yahoo.com>
2018-11-26 14:35:16 -0700

    Initial creation of the map.

---
+++ src/Components/Maps.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import GoogleMapReact from 'google-map-react'
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class Maps extends Component {
+  static defaultProps = {
+    center: {
+      lat: 39.742043,
+      lng: -104.991531
+    },
+    zoom: 11
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyBMyIR5up1KiKHZzvm6N7xAxm8eREIDpCM' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={39.742043}
+            lng={-104.991531}
+            text={''}
+          />
+        </GoogleMapReact>
+      </div>
+    )
+  }
+}
+
+export default Maps
