fc032f997cfa2124a6d1f272cfd676692e736791
bill <williamandrewbain@gmail.com>
2018-11-27 15:15:14 -0700

    adding seed data

--- backend/app.py
+++ backend/app.py
@@ -45,6 +45,21 @@ class Bars(db.Model):
     #     return '<User %r>' % self.username
 
 
+# user1 = People('bill', 'bain', 'b@gmail.com', 'billb', 'pass123')
+# user2 = People('Evan', 'Apodaca', 'evapodaca87@gmail.com',
+#                'evapodaca87', 'evan')
+# user3 = People('Jody', 'Isaguirre', 'jody@mail.com', 'jodyisaquirre', 'irre')
+# user4 = People('Jonathan', 'Hovland', 'jonathan@mail.com', 'jonathanhovland',
+#                'land')
+# user5 = People('Rob', 'Hill', 'robhill@mail.com', 'robhill', 'hill')
+# db.session.add(user1)
+# db.session.add(user2)
+# db.session.add(user3)
+# db.session.add(user4)
+# db.session.add(user5)
+# db.session.commit()
+
+
 # routes
 @app.route('/')
 def index():
@@ -68,9 +83,10 @@ def post_user():
     return '🐍'
 
 
-@app.route('/about')
-def about():
-    return 'Find out what we\'re about'
+@app.route('/users')
+def users():
+    # myPeople = People.query.all()
+    return print('heyo')
 
 
 # run in debug mode
