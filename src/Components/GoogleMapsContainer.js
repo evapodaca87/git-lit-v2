b66be03b40c975114614e43d4ecf0784c56b37d6
jonathanhovland <jonathan.hovland@yahoo.com>
2018-11-27 08:34:06 -0700

    Geolocation is working and obtaining your current loaction

--- src/Components/GoogleMapsContainer.js
+++ src/Components/GoogleMapsContainer.js
@@ -1,16 +1,33 @@
 import React from 'react'
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react'
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react'
 
 class GoogleMapsContainer extends React.Component{
 
-  constructor(props) {
-      super(props);
-      this.state = {
+ state = {
         showingInfoWindow: false,
         activeMarker: {},
         selectedPlace: {},
         initialCenter: { lat: 39.75752, lng: -105.00687 }
-      }
+  }
+
+  async componentDidMount() {
+    const { lat, lng } = await this.getcurrentLocation()
+    this.setState({ initialCenter: { lat, lng }})
+  }
+
+   getcurrentLocation() {
+    if (navigator && navigator.geolocation) {
+      return new Promise((resolve) => {
+        navigator.geolocation.getCurrentPosition(position => {
+          const coords = position.coords
+          resolve({
+            lat: coords.latitude,
+            lng: coords.longitude
+          })
+        })
+      })
+    }
+    return { lat: 39.75752, lng: -105.00687 }
   }
 
   onMarkerClick = (props, marker, e) => {
@@ -36,34 +53,33 @@ render() {
     'marginLeft': 'auto',
     'marginRight': 'auto'
   }
-    {
-      return (
-        <Map
-        item
-        xs = { 12 }
-        style = { style }
-        google = { this.props.google }
-        onClick = { this.onMapClick }
-        zoom = { 14 }
-        initialCenter = {this.state.initialCenter}
+    
+    return (
+      <Map
+      item
+      xs = { 12 }
+      style = { style }
+      google = { this.props.google }
+      onClick = { this.onMapClick }
+      zoom = { 14 }
+      initialCenter = {this.state.initialCenter}
 
-      >
-        <Marker
-          onClick = { this.onMarkerClick }
-          title = { 'Changing Colors Garage' }
-          position = {this.state.initialCenter}
-          name = { 'Changing Colors Garage' }
-        />
-          <InfoWindow
-            marker = { this.state.activeMarker }
-            visible = { this.state.showingInfoWindow }>
-            <div>
-              <h3>Current Location</h3>
-            </div>
-          </InfoWindow>
-        </Map>
-      );
-    }
+    >
+      <Marker
+        onClick = { this.onMarkerClick }
+        title = { 'Changing Colors Garage' }
+        position = {this.state.initialCenter}
+        name = { 'Changing Colors Garage' }
+      />
+        <InfoWindow
+          marker = { this.state.activeMarker }
+          visible = { this.state.showingInfoWindow }>
+          <div>
+            <h3>Current Location</h3>
+          </div>
+        </InfoWindow>
+      </Map>
+    )
   }
 }
 
