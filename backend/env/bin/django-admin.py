bfc8e844e065185b5112afb66547299a55e80a27
bill <williamandrewbain@gmail.com>
2018-11-28 12:07:11 -0700

    reworking backend

---
+++ backend/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/williambain/playground/Projects/git-lit-v2/backend/env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
