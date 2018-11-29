35fd547d4bfc01c345974c4a001d93e66e6d12d1
bill <williamandrewbain@gmail.com>
2018-11-29 11:04:27 -0700

    Add working links to login

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,43 +1,47 @@
-import React, { Component } from 'react';
-import { Button, Form, Segment } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Button, Form, Segment } from "semantic-ui-react";
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
-    
 
+class Login extends Component {
 
- class Login extends Component{
-   constructor(props){
-     super(props);
+  constructor(props) {
+    super(props);
 
-     this.state={
-       email:'',
-       password:''
-          }
-   }
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
 
-   render(){
-   return(
-<div>
-    <Segment inverted>
-        <Form inverted>
-            <Form.Group widths='equal'>
+  render() {
+    return (
+      <div>
+        <Segment inverted>
+          <Form inverted>
+            <Form.Group widths="equal">
+              <Form.Input
+                type="email"
+                label="Email"
+                placeholder="joe@schmoe.com"
+                onChange={event => this.setState({ email: event.target.value })}
+              />
 
-                <Form.Input type="email" label='Email' placeholder='joe@schmoe.com'
-                onChange={event =>this.setState({email: event.target.value})} />
+              <Form.Input
+                type="password"
+                label="Password"
+                placeholder="Password"
+                onChange={event =>
+                  this.setState({ password: event.target.value })
+                }
+              />
 
-                <Form.Input type="password" label='Password' placeholder='Password'
-                onChange={event =>this.setState({password: event.target.value})} />
             </Form.Group>
-            <Button type='submit'>Submit</Button>
-        </Form>
-
-    </Segment>
-
-    
-    </div>
-  )
-
+            <Button type="submit"><Link to="/Home">Home</Link></Button>
+          </Form>
+        </Segment>
+      </div>
+    );
   }
-
-  }
-;
+}
 export default Login;
