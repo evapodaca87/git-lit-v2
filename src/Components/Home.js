718e3e8a3c6d7543b3c35040869c629b0a910d24
evapodaca87 <evapodaca87@gmail.com>
2018-11-29 13:40:36 -0700

    updated fire toggle

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,92 +1,102 @@
-import React, { Component } from "react";
-import "../App.css";
-import Slide from "../Components/slide.js";
-import { Rating } from "semantic-ui-react";
-import GoogleMapsContainer from "../Components/GoogleMapsContainer.js";
-import { Checkbox } from "semantic-ui-react";
-import { Link } from 'react-router-dom';
-import anh from '../pictures/anh.png'
+import React, { Component } from 'react';
+import '../App.css';
+import Slide from '../Components/slide.js';
+import { Rating, Button, Divider, Image, Transition } from 'semantic-ui-react';
+import GoogleMapsContainer from '../Components/GoogleMapsContainer.js';
+import { Checkbox } from 'semantic-ui-react';
 
 class Home extends Component {
-  state = {
-    showMap: false,
-    initialCenter: {
-      lat: 39,
-      lng: -104
-    }
-  };
-
-  async componentDidMount() {
-    const coords = await this.getCurrentLocation();
-    console.log("coords", coords);
-  }
-
-  getCurrentLocation = () => {
-    if (navigator && navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        position => {
-          this.setState({
-            initialCenter: {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            }
-          });
+    state = {
+        showMap: false,
+        initialCenter: {
+            lat: 39,
+            lng: -104
         },
-        error => {
-          console.log(error);
-        }
-      );
-    } else {
-      return { lat: 27, lng: -100 };
+
+        visible: false
+    };
+    toggleVisibility = () => this.setState({ visible: !this.state.visible });
+
+    async componentDidMount() {
+        console.log('component did mount ran');
+        const coords = await this.getCurrentLocation();
+        console.log('coords', coords);
     }
-  };
 
-  toggleMap = () => {
-    this.setState({ showMap: !this.state.showMap });
-  };
+    getCurrentLocation = () => {
+        if (navigator && navigator.geolocation) {
+            console.log('getting location');
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    console.log('getCurrentPosition running (location callback)');
+                    this.setState({
+                        initialCenter: {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude
+                        }
+                    });
+                    console.log('initial center set', this.state.initialCenter);
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+        } else {
+            console.log('initial center not set');
+            return { lat: 27, lng: -100 };
+        }
+    };
 
-  render() {
-    const showMap = this.state.showMap;
-    return (
-      <div className="App2">
-        <header>
-        <Link to="/Profile"><img
-            alt="profile"
-            className="logoHead profile-pic"
-            src={anh}
-          />
-        </Link>
+    toggleMap = () => {
+        this.setState({ showMap: !this.state.showMap });
+    };
 
-          <Checkbox
-            toggle
-            checked={this.state.showMap}
-            onChange={this.toggleMap}
-          />
+    render() {
+        const { visible } = this.state;
+        const showMap = this.state.showMap;
+        return (
+            <div className='App2'>
+                <header>
+                    <img className='logoHead' src='/pictures/gitlit.png' />
 
-          <img
-            alt="logo"
-            className="logoHead"
-            src="https://files.slack.com/files-pri/T1T555TL0-FEBL8TWRX/screen_shot_2018-11-26_at_11.26.57_am.png"
-          />
-        </header>
+                    <div className='fire'>
+                        <Checkbox
+                            toggle
+                            content={visible ? 'Hide' : 'Show'}
+                            onClick={this.toggleVisibility}
+                            checked={this.state.showMap}
+                            onChange={this.toggleMap}
+                        />
+                        <Divider hidden />
+                    </div>
+                    <div className='fire'>
+                        {visible ? (
+                            <Image className='image' style={{ background: `url(../pictures/fire2.png` }} />
+                        ) : (
+                            <Image className='image' style={{ background: `url(../pictures/fire1.png` }} />
+                        )}
+                    </div>
+                    <img
+                        className='logoHead'
+                        src='https://files.slack.com/files-pri/T1T555TL0-FEBL8TWRX/screen_shot_2018-11-26_at_11.26.57_am.png'
+                    />
+                </header>
 
-        {showMap && (
-          <GoogleMapsContainer initialCenter={this.state.initialCenter} />
-        )}
-        <Slide />
-        <h3>Bar Review</h3>
-        <div className="barReview">
-          <p> Bar Name</p>
-          <p> This bar was a total piece of shit. It's no village in. </p>
-          <div className="rating">
-            <i class="trash icon" />
-            <Rating icon="star" defaultRating={3} maxRating={5} />
-            <i class="fire icon" />
-          </div>
-        </div>
-      </div>
-    );
-  }
+                {showMap && <GoogleMapsContainer initialCenter={this.state.initialCenter} />}
+                <Slide />
+                <h3>Bar Review</h3>
+                <div className='barReview'>
+                    <p> Bar Name</p>
+                    <p> This bar was a total piece of shit. It's no village inn. </p>
+                    <div className='rating'>
+                        <i class='trash icon' />
+                        <Rating icon='star' defaultRating={3} maxRating={5} />
+                        <i class='fire icon' />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Home;
--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,34 +1,32 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import '../App.css';
-import anh from '../pictures/anh.png';
 
 class Profile extends Component {
     render() {
         return (
             <div className='App3'>
                 <header>
-                    <Link to='/Home'>
-                        <img
-                            className='logoHead profile-pic'
-                            alt='back2home'
-                            src='https://files.slack.com/files-pri/T1T555TL0-FEBL8TWRX/screen_shot_2018-11-26_at_11.26.57_am.png'
-                        />
-                    </Link>
+                    <img
+                        className='logoHead'
+                        src='https://files.slack.com/files-pri/T1T555TL0-FEBL8TWRX/screen_shot_2018-11-26_at_11.26.57_am.png'
+                    />
                 </header>
                 <div>
-                    <img alt="logo" className='logoHead profile-pic' src={anh} />
+                    <img
+                        className='logoHead'
+                        src='http://profilepicturesdp.com/wp-content/uploads/2018/07/profile-default-picture-4.png'
+                    />
                 </div>
                 <div className='Profilecontainer'>
                     <div>
-                        <p>Anh Nguyen</p>
+                        <p>Name</p>
                     </div>
-                    <p>"My life's dope & I do dope shit"</p>
+                    <p>Tagline</p>
                 </div>
                 <div>
                     <h1>GitLit History</h1>
                 </div>
-                <img alt="pic" src='https://via.placeholder.com/800x700' />
+                <img src='https://via.placeholder.com/800x700' />
             </div>
         );
     }
