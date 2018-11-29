cb82fadfa0aa65ee596146c8a54ca04d0d48be19
evapodaca87 <evapodaca87@gmail.com>
2018-11-29 11:43:29 -0700

    hardcoded user1 for presentation

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,38 +1,35 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
-
-
+import anh from '../pictures/anh.png';
 
 class Profile extends Component {
     render() {
         return (
             <div className='App3'>
                 <header>
-                <Link to="/Home">
-                    <img
-                        className='logoHead profile-pic'
-                        alt='back2home'
-                        src='https://files.slack.com/files-pri/T1T555TL0-FEBL8TWRX/screen_shot_2018-11-26_at_11.26.57_am.png' />
-                </Link>
+                    <Link to='/Home'>
+                        <img
+                            className='logoHead profile-pic'
+                            alt='back2home'
+                            src='https://files.slack.com/files-pri/T1T555TL0-FEBL8TWRX/screen_shot_2018-11-26_at_11.26.57_am.png'
+                        />
+                    </Link>
                 </header>
                 <div>
-                <img
-                    className='logoHead'
-                    src='http://profilepicturesdp.com/wp-content/uploads/2018/07/profile-default-picture-4.png' />
+                    <img className='logoHead profile-pic' src={anh} />
                 </div>
-                <div className="Profilecontainer">
+                <div className='Profilecontainer'>
+                    <div>
+                        <p>Anh Nguyen</p>
+                    </div>
+                    <p>"My life's dope & I do dope shit"</p>
+                </div>
                 <div>
-                    <p>Name</p>
-                  </div>
-                  <p>Tagline</p>
-
-                  </div>
-                  <div>
                     <h1>GitLit History</h1>
-                  </div>
-                  <img src="https://via.placeholder.com/800x700" />
                 </div>
+                <img src='https://via.placeholder.com/800x700' />
+            </div>
         );
     }
 }
