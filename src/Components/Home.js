6c7eb1b67d09192f804da6bc55ec63c546101d9a
evapodaca87 <evapodaca87@gmail.com>
2018-11-29 14:44:43 -0700

    fixed fire toggle button

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,101 +1,70 @@
-import React, { Component } from 'react';
-import '../App.css';
-import Slide from '../Components/slide.js';
-import { Rating, Button, Divider, Image, Transition } from 'semantic-ui-react';
-import GoogleMapsContainer from '../Components/GoogleMapsContainer.js';
-import { Checkbox } from 'semantic-ui-react';
+import React, { Component } from 'react'
+import '../App.css'
+import GoogleMapsContainer from '../Components/GoogleMapsContainer.js'
+import { Button } from 'semantic-ui-react'
+import fire1 from '../pictures/fire1.png'
+import fire2 from '../pictures/fire2.png'
+
 
 class Home extends Component {
+
     state = {
         showMap: false,
+        checked: true,
         initialCenter: {
-            lat: 39,
-            lng: -104
-        },
-
-        visible: false
-    };
-    toggleVisibility = () => this.setState({ visible: !this.state.visible });
-
-    async componentDidMount() {
-        console.log('component did mount ran');
-        const coords = await this.getCurrentLocation();
-        console.log('coords', coords);
+            lat:39 ,
+            lng:-104
+          }
     }
 
-    getCurrentLocation = () => {
+    async componentDidMount() {
+        const coords = await this.getCurrentLocation()
+        console.log("coords",coords)
+      }
+
+       getCurrentLocation = () => {
         if (navigator && navigator.geolocation) {
-            console.log('getting location');
             navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    console.log('getCurrentPosition running (location callback)');
-                    this.setState({
-                        initialCenter: {
-                            lat: position.coords.latitude,
-                            lng: position.coords.longitude
-                        }
-                    });
-                    console.log('initial center set', this.state.initialCenter);
-                },
-                (error) => {
-                    console.log(error);
-                }
-            );
-        } else {
-            console.log('initial center not set');
-            return { lat: 27, lng: -100 };
+              position => {
+                this.setState({initialCenter:{
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude}
+                })
+              }, error =>
+                {console.log(error)}
+            )
+          }
+        else{
+          return { lat: 27, lng: -100 }
         }
-    };
+      }
 
     toggleMap = () => {
-        this.setState({ showMap: !this.state.showMap });
-    };
+        this.setState({showMap: !this.state.showMap, checked: !this.state.checked})
+    }
 
     render() {
-        const { visible } = this.state;
-        const showMap = this.state.showMap;
+        const showMap = this.state.showMap
         return (
             <div className='App2'>
                 <header>
-                    <img className='logoHead' src='/pictures/gitlit.png' />
+                    <img
+                        className='logoHead'
+                        src='http://profilepicturesdp.com/wp-content/uploads/2018/07/profile-default-picture-4.png'
+                    />
+                     
+                     {/* <Checkbox toggle checked={this.state.showMap} onChange={this.toggleMap}/> */}
+        <Button  id='litButton' toggle checked={this.state.showMap} onClick={this.toggleMap}toggle>{this.state.checked ? <img className='litFire'src={fire1} /> : <img className='litFire'src={fire2} />}</Button>
 
-                    <div className='fire'>
-                        <Checkbox
-                            toggle
-                            content={visible ? 'Hide' : 'Show'}
-                            onClick={this.toggleVisibility}
-                            checked={this.state.showMap}
-                            onChange={this.toggleMap}
-                        />
-                        <Divider hidden />
-                    </div>
-                    <div className='fire'>
-                        {visible ? (
-                            <Image className='image' style={{ background: `url(../pictures/fire2.png` }} />
-                        ) : (
-                            <Image className='image' style={{ background: `url(../pictures/fire1.png` }} />
-                        )}
-                    </div>
                     <img
                         className='logoHead'
                         src='https://files.slack.com/files-pri/T1T555TL0-FEBL8TWRX/screen_shot_2018-11-26_at_11.26.57_am.png'
                     />
                 </header>
-
                 {showMap && <GoogleMapsContainer initialCenter={this.state.initialCenter} />}
-                <Slide />
-                <h3>Bar Review</h3>
-                <div className='barReview'>
-                    <p> Bar Name</p>
-                    <p> This bar was a total piece of shit. It's no village inn. </p>
-                    <div className='rating'>
-                        <i class='trash icon' />
-                        <Rating icon='star' defaultRating={3} maxRating={5} />
-                        <i class='fire icon' />
-                    </div>
-                </div>
-            </div>
-        );
+        </div>
+        
+        )
     }
 }
 
